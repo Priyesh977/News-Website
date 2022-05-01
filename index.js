e5e84e3c44a0c72c29fbc9245b2b20e09696d84a
Priyesh977 <80889959+Priyesh977@users.noreply.github.com>
2022-05-01 16:58:26 +0530

    GNewsAPI Included
    News Source changed from NEWSAPI to GNews and removed extra some buttons.

--- index.js
+++ index.js
@@ -1,74 +1,86 @@
 console.log("This is my index js file");
 
 // Initialize the news api parameters
-let apiKey = "015032c85150475b9510137a646fb8d8";
+let apiKey = "99a75cd5cc6c90e13af94e1042482598";
 
 // Grabbing the buttons by their unique IDs
+let world = document.getElementById("world");
 let india = document.getElementById("india");
 let usa = document.getElementById("usa");
-let korea = document.getElementById("sKorea");
-let ukraine = document.getElementById("ukraine");
-let newZealand = document.getElementById("newZealand");
+// let korea = document.getElementById("sKorea");
+// let ukraine = document.getElementById("ukraine");
+// let newZealand = document.getElementById("newZealand");
 let australia = document.getElementById("australia");
 let ipl = document.getElementById("ipl");
 
-// Populating the webpage for the first time with Indian News as default
-let https = `https://newsapi.org/v2/everything?q=India&apiKey=${apiKey}`;
+// Populating the webpage for the first time with World News as default
+let https = `https://gnews.io/api/v4/search?q=example&token=${apiKey}`;
 ajaxRequest(https);
+
+// Getting news for World
+world.addEventListener("click", () => {
+  let https = `https://gnews.io/api/v4/search?q=example&token=${apiKey}`;
+  ajaxRequest(https);
+});
 
 // Getting news for India
 india.addEventListener("click", () => {
   let country = "in";
-  let https = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+  let https = `https://gnews.io/api/v4/top-headlines?q="India"&token=${apiKey}&country=${country}&lang=en`;
   ajaxRequest(https);
 });
 
 // Getting news for USA
 usa.addEventListener("click", () => {
   let country = "us";
-  let https = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+  let https = `https://gnews.io/api/v4/top-headlines?q=example&token=${apiKey}&country=${country}&lang=en`;
   ajaxRequest(https);
 });
 
 // Getting news for IPL
 ipl.addEventListener("click", () => {
   let country = "in";
-  let https = `https://newsapi.org/v2/top-headlines?country=${country}&q=ipl&apiKey=${apiKey}`;
-  ajaxRequest(https);
-});
-
-// Getting news for New Zealand
-newZealand.addEventListener("click", () => {
-  let country = "nz";
-  let https = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+  let https = `https://gnews.io/api/v4/top-headlines?q="IPL"&token=${apiKey}&country=${country}&lang=en&topic=sports`;
   ajaxRequest(https);
 });
 
 // Getting news for Australia
 australia.addEventListener("click", () => {
   let country = "au";
-  let https = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+  let https = `https://gnews.io/api/v4/top-headlines?q=example&token=${apiKey}&country=${country}&lang=en`;
   ajaxRequest(https);
 });
 
-// Getting news for South Korea
-korea.addEventListener("click", () => {
-  let country = "kr";
-  let https = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
-  ajaxRequest(https);
-});
+// // Getting news for New Zealand
+// newZealand.addEventListener("click", () => {
+//   let country = "nz";
+//   let https = `https://gnews.io/api/v4/top-headlines?q=example&token=${apiKey}&country=${country}&lang=en`;
+//   ajaxRequest(https);
+// });
 
-// Getting news for Ukraine
-ukraine.addEventListener("click", () => {
-  let country = "ua";
-  let https = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
-  ajaxRequest(https);
-});
+// // Getting news for South Korea
+// korea.addEventListener("click", () => {
+//   let country = "ru";
+//   let https = `https://gnews.io/api/v4/top-headlines?q=example&token=${apiKey}&country=${country}`;
+//   ajaxRequest(https);
+// });
+
+// // Getting news for Ukraine
+// ukraine.addEventListener("click", () => {
+//   let country = "ua";
+//   let https = `https://gnews.io/api/v4/search?q=example&token=${apiKey}&country=${country}`;
+//   ajaxRequest(https);
+// });
 
 // Grab the news container
 let newsAccordion = document.getElementById("newsAccordion");
 
 // Trial URL for getting different news
+// *************************GNewsAPI News Source*******************************************
+// https://gnews.io/api/v4/top-headlines?q=example&token=${apiKey}&country=${country}&lang=en
+
+
+// *************************NewsAPI News Source****************************************
 // https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY
 // https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
 
